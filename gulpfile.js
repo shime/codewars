d06a4873ffb70c47d35251e700b724e304f307f2
Hrvoje Šimić <shime.ferovac@gmail.com>
2014-05-18 00:17:18 +0200

    create separate task for integration tests

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,17 @@ var gulp = require("gulp");
 var mocha = require("gulp-mocha");
 
 gulp.task("default", function(){
-  return gulp.src(["test/*_test.js"], { read: false })
+  return gulp.src(["test/*_test.js", "!test/integration/*_test.js"], { read: false })
+             .pipe(mocha({
+               reporter: "spec",
+               globals: {
+                 expect: require("expect.js")
+               }
+             }));
+});
+
+gulp.task("test:all", function(){
+  return gulp.src(["test/integration/*_test.js"], { read: false })
              .pipe(mocha({
                reporter: "spec",
                globals: {
