7c547c1625f4bf20af554f6c24f447a9cfd90b20
Hrvoje Šimić <shime.ferovac@gmail.com>
2014-05-17 23:02:41 +0200

    better articulation for the exploration test

--- test/exploration_test.js
+++ test/exploration_test.js
@@ -2,22 +2,24 @@ var expect = require("expect.js"),
     request = require("request"),
     fs = require("fs");
 
-describe("exploring the codewars API", function(){
-  it("is able to browse katas", function(done){
-    this.timeout(10000);
+describe("codewars API", function(){
+  describe("browsing katas", function(){
+    it("has the following URL: /trainer/peek/javascript/reference_workout", function(done){
+      this.timeout(10000);
 
-    var options = {
-      url: "http://www.codewars.com/trainer/peek/javascript/reference_workout?dequeue=true",
-      headers: {
-        "Cookie": "remember_user_token=" + process.env["CODEWARS_REMEMBER_USER_TOKEN"] + ";"
+      var options = {
+        url: "http://www.codewars.com/trainer/peek/javascript/reference_workout",
+        headers: {
+          "Cookie": "remember_user_token=" + process.env["CODEWARS_REMEMBER_USER_TOKEN"] + ";"
+        }
       }
-    }
 
-    request(options, function(error, response, body){
-      done();
-      var info = JSON.parse(body);
-      expect(info.name).to.not.be.empty();
-      expect(info.description).to.not.be.empty();
+      request(options, function(error, response, body){
+        done();
+        var info = JSON.parse(body);
+        expect(info.name).to.not.be.empty();
+        expect(info.description).to.not.be.empty();
+      });
     });
   });
 });
