b93002bafb69bafe98886101995e067bec7e9678
Hrvoje Šimić <shime.ferovac@gmail.com>
2014-08-10 01:51:46 +0200

    fix travis

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,8 @@
-var gulp = require("gulp");
-var mocha = require("gulp-mocha");
+var gulp = require("gulp"),
+    mocha = require("gulp-mocha");
 
 gulp.task("default", function(){
   return gulp.src(["test/*_test.js", "!test/integration/*_test.js"], { read: false })
-             .pipe(mocha({
-               reporter: "spec",
-               globals: {
-                 expect: require("expect.js")
-               }
-             }));
-});
-
-gulp.task("test:all", function(){
-  return gulp.src(["test/**/*_test.js"], { read: false })
              .pipe(mocha({
                reporter: "spec",
                globals: {
--- test/filesystem_test.js
+++ test/filesystem_test.js
@@ -4,8 +4,9 @@ var expect = require("expect.js"),
 
 describe("Codewars.setup", function(){
   it("writes passed settings to ~/.config/codewars/settings.json", function(){
-    var settings = {token: "foo", language: "bar"};
+    var settings = {token: "foo", language: "bar", strategy: 'default'};
     var fsStub = {
+      existsSync: function(path){},
       writeFile: function(path, data) {
         expect(path).to.be(Codewars().paths.settings + "settings.json");
         expect(data).to.be(JSON.stringify(settings));
